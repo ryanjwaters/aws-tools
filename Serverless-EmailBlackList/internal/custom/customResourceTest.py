5a33a16b589add9275414bc87d1e05453e0a2d04
ryanjwaters <ryanjwaters@gmail.com>
2020-05-12 16:24:53 -0400

    Cleanup the code for custom resource example

--- Serverless-EmailBlackList/internal/custom/customResourceTest.py
+++ Serverless-EmailBlackList/internal/custom/customResourceTest.py
@@ -1,83 +1,66 @@
 import boto3
 import json
-import random
+import logging
 from botocore.vendored import requests
-from collections import namedtuple
 from botocore.exceptions import ClientError
 
-def test(ec2TagName):
-
-    try:
-        #ec2 = boto3.resource('ec2') #, region_name=Region
-        #ec2.create_instances(
-        #    ImageId='ami-0323c3dd2da7fb37d',
-        #    MinCount=1,
-        #    MaxCount=1,
-        #    InstanceType='t2.micro')
-
-        return {
-            "StatusCode" : 200,
-            "Message" : "OK"
-        }
-
-    except ClientError as e:
-        return {
-            "StatusCode" : 400,
-            "Message" : e
-        }
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 def handler(event, context):
 
-    # Create variable to hold the response
-    print("Custom Resource: Request received")
-    print(event)
-    for k,v in event.items():
-        print(f"Custom Resource: key: {k} value: {v}")
+    try:
+        LOGGER.info('Request received:\n %s', event)
+        LOGGER.info('Response received:\n %s', context)
+
+        # Retrieve data from the request (ie-get EC2 tag name to launch instance)
+        request_properties = event.get('ResourceProperties', None)
+        ec2TagName = request_properties['ec2TagName'] #Example property sent in from CFM
+
+        if event['RequestType'] == 'Create':
+            LOGGER.info('Create custom resource')
+            send_response(event, context, "SUCCESS", { "Message": "Resource created" })
+        
+        elif event['RequestType'] == 'Update':
+            LOGGER.info('Update custom resource')
+            send_response(event, context, "SUCCESS", { "Message": "Resource updated" })
+        
+        elif event['RequestType'] == 'Delete':
+            LOGGER.info('Delete custom resource')
+            send_response(event, context, "SUCCESS", { "Message": "Resource deleted" })
+        
+        else:
+            LOGGER.info('Unexpected event request received from CFM')
+            send_response(event, context, "FAILED", { "Message": "Unexpected event received from CFM" })
     
-    # Retrieve data from the request
-    request_type = event.get('RequestType', None)
-    resource_type = event.get('ResourceType', None)
-    response_url = event.get('ResponseURL', None)
-    request_properties = event.get('ResourceProperties', None)
-    ec2TagName = request_properties['ec2TagName']
+    except:
+        LOGGER.error('Unexpected error during processing', exc_info=True)
+        send_response(event, context, "FAILED", { "Message": "Exception during processing" })
 
-    # These values must be copied to the response
-    response_data = {}
-    response_data['StackId'] = event.get('StackId', None)
-    response_data['RequestId'] = event.get('RequestId', None)
-    response_data['LogicalResourceId'] = event.get('LogicalResourceId', "")
-    response_data['PhysicalResourceId'] = event.get('PhysicalResourceId', f"{context.function_name}-{context.function_version}")
-    response_data['Data'] = {}
-    response_data['Reason'] = "More info in CloudWatch"
-
-    answer = test(ec2TagName)
-
-    if answer['StatusCode'] == 400:
-        print(f"Custom Resource: FAILED: {answer['Message']}")
-        response_data['Status'] = "FAILED"
-        response_data['Reason'] = answer['Message']
-    else:
-        response_data['Status'] = "SUCCESS"
-        response_data['Data']['Test'] = answer['Message']
-        response_data['Reason'] = answer['Message']
+def send_response(event, context, response_status, response_data):
 
     # Save the response in S3 using the responseURL
-    json_response = json.dumps(response_data)
-    headers = {
-        'content-type': '',
-        'content-length': str(len(json_response))
-    }
-
-    print("Custom Resource: Sending response")
-    print(json_response)
-
     try:
-        result = {}
-        result = requests.put(response_url, data=json_response, headers=headers)
-        print(f"CloudFormation returned status code: {result.reason}")
-    except Exception as e:
-        print(f"Failed sending response: error: {e}")
-        raise
+        response_body = json.dumps({
+            "StackId": event['StackId'],
+            "RequestId": event['RequestId'],
+            "LogicalResourceId": event['LogicalResourceId'],
+            "Status": response_status,
+            "Reason": "See CW: " + context.log_stream_name,
+            "PhysicalResourceId": context.log_stream_name,
+            "Data": response_data
+        })
 
-    # Return the response data
-    return response_data
+        LOGGER.info("Custom Resource: Sending response %s", response_body)
+
+        headers = {
+            'content-type': '',
+            'content-length': str(len(response_body))
+        }
+
+        response_url = event.get('ResponseURL', None)
+        result = requests.put(response_url, data=response_body, headers=headers)
+        LOGGER.info("Returned status code: %s", result.reason)
+    except Exception as e:
+        LOGGER.error("Failed sending response", exc_info=True)
+        raise
